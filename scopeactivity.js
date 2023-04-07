53e4e73f427e9825c174b674528222b394c58f93
Karen Ngo <uminhngo@gmail.com>
2023-04-07 12:38:19 -0400

    added scope practice

---
+++ scopeactivity.js
@@ -0,0 +1,21 @@
+function outer(){
+    let a = 'I am a string.';
+    let b = {name: 'Object'};
+
+    console.log('outer a -', a);
+    console.log('outer b -', b);
+  
+    function inner(a,b) {
+        a = 'Is this right?';
+        b.size = 'small';
+
+        console.log('inner a -', a);
+        console.log('inner b -', b);
+    }
+    inner(a,b);
+
+    console.log('bouncy a -', a);
+    console.log('bouncy b', b);
+}
+  
+  outer();
