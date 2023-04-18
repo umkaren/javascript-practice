b634f9b8931c6975755e59d6bf015de0e42712b0
Karen Ngo <uminhngo@gmail.com>
2023-04-18 18:06:35 -0400

    updated removal event

--- browsercart/cartApp.js
+++ browsercart/cartApp.js
@@ -1,58 +1,61 @@
 // Create a function called initializeCart that checks if there's a cart in localStorage; if not, it initializes an empty cart and stores it in localStorage.
 function initializeCart() {
-  let cart = JSON.parse(localStorage.getItem('cart'));
-  if (!cart) {
-    cart = [];
-    localStorage.setItem('cart', JSON.stringify(cart));
-  }
+    if (!localStorage.getItem('cart')) {
+        localStorage.setItem('cart', JSON.stringify([]));
+    };
 };
 
 // Create a function called addItem that takes an item object as a parameter, retrieves the cart from localStorage, adds the item to the cart, and then stores the updated cart back into localStorage.
 function addItem(item) {
-  let cart = JSON.parse(localStorage.getItem('cart'));
-  cart.push(item);
-  localStorage.setItem('cart', JSON.stringify(cart));
+    let cart = JSON.parse(localStorage.getItem('cart'));
+    cart.push(item);
+    localStorage.setItem('cart', JSON.stringify(cart));
 };
 
 // Create a function called removeItem that takes an item id as a parameter, retrieves the cart from localStorage, removes the item with the given id from the cart, and then stores the updated cart back into localStorage.
-function removeItem(itemId) {
-  let cart = JSON.parse(localStorage.getItem('cart'));
-  let newCart = cart.filter(item => item.id !== itemId);
-  localStorage.setItem('cart', JSON.stringify(newCart));
+function removeItem(id) {
+    let cart = JSON.parse(localStorage.getItem('cart'));
+    let index = cart.findIndex(item => item.id === id);
+    if (index !== -1) {
+        cart.splice(index, 1);
+        localStorage.setItem('cart', JSON.stringify(cart));
+    };
 };
 
 // Create a function called displayCart that retrieves the cart from localStorage and logs the cart contents to the console.
 function displayCart() {
-  let cart = JSON.parse(localStorage.getItem('cart'));
-  console.log(cart);
-}
+    let cart = JSON.parse(localStorage.getItem('cart'));
+    console.log(cart);
+};
 
 // Create event listeners for the add item form and display cart button to call the respective functions.
 const addItemForm = document.getElementById('addItemForm');
 
+// add item event
 const addButton = document.getElementById('addButton');
-addButton.addEventListener('click', function() { 
-  const itemName = addItemForm.elements.itemName.value;
-  const itemPrice = addItemForm.elements.itemPrice.value;
-  const item = {
-    id: Date.now(),
-    name: itemName,
-    price: Number(itemPrice)
-  };
-  addItem(item);
-  addItemForm.reset();
+addItemForm.addEventListener('submit', function(event) {
+    event.preventDefault(); 
+    const itemName = addItemForm.elements.itemName.value;
+    const itemPrice = addItemForm.elements.itemPrice.value;
+    const item = {id: Date.now(), name: itemName, price: Number(itemPrice)};
+    addItem(item);
+    addItemForm.reset();
 });
 
-// remove item event. giving up on this tbh
+// remove item event
 const removeButton = document.getElementById('removeButton')
-removeButton.addEventListener('click', function(){
-    removeItem();
-    addItemForm.reset();
-})
+removeButton.addEventListener('click', function(event){
+    event.preventDefault();
+    const itemID = prompt('Enter item ID to remove:');
+    if (itemID) {
+        removeItem(parseInt(itemID))};
+    displayCart();
+});
 
+//displays cart in console
 const displayCartButton = document.querySelector('#displayCartButton');
 displayCartButton.addEventListener('click', function() {
-  displayCart();
+    displayCart();
 });
 
 initializeCart();
